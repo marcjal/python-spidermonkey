a0a53bce94c8aa4f8c5bdbe38707a21e19459742
varmaa <varmaa@e13cad08-5a4f-0410-891f-9d64462cff50>
2008-06-11 03:16:39 +0000

    Removed SConstruct, updated setup.py to optionally work w/o Pyrex.

--- trunk/setup.py
+++ trunk/setup.py
@@ -1,19 +1,40 @@
 import os
-macros = []
-# I've only tested on Linux, so this is guesswork
-if os.name == "posix":
-    symbol = "XP_UNIX"
-elif os.name == "nt":
-    symbol = "XP_PC"
-elif os.name == "mac":
-    symbol = "XP_MAC"
-    # what's XP_MAC_MPW?
-elif os.name == "os2":
-    symbol = "XP_OS2"
-macros.append((symbol, None))
-
+import sys
 from distutils.core import setup, Extension
-from Pyrex.Distutils import build_ext
+
+try:
+    # For some reason, importing Pyrex appears to change the value of
+    # the MACOSX_DEPLOYMENT_TARGET to something that may be
+    # inaccurate.  So we'll make sure that it's set to whatever it was
+    # before we imported Pyrex.
+    oldTarget = os.environ["MACOSX_DEPLOYMENT_TARGET"]
+
+    from Pyrex.Distutils import build_ext
+
+    os.environ["MACOSX_DEPLOYMENT_TARGET"] = oldTarget
+
+    sources = ["spidermonkey.pyx"]
+except ImportError:
+    from distutils.command.build_ext import build_ext
+    sources = ["spidermonkey.c"]
+
+if not os.environ.has_key("MOZSDKDIR"):
+    print ("Please set the MOZSDKDIR environment variable "
+           "to the location of your XULRunner SDK before "
+           "running this script.")
+    sys.exit(1)
+
+MOZ_SDK_DIR = os.environ["MOZSDKDIR"]
+
+ext = Extension(
+    "spidermonkey",
+    sources=sources,
+    include_dirs=[os.path.join(MOZ_SDK_DIR, "include"),
+                  os.path.join(MOZ_SDK_DIR, "include", "js")],
+    library_dirs=[os.path.join(MOZ_SDK_DIR, "lib")],
+    extra_compile_args=["-include", "mozilla-config.h"],
+    libraries=["mozjs"]
+    )
 
 setup(name = "spidermonkey",
       version = "0.0.1a",
@@ -21,7 +42,7 @@ setup(name = "spidermonkey",
       author = "John J. Lee",
       author_email = "jjl@pobox.com",
       description = "JavaScript / Python bridge.",
-      url = "http://wwwsearch.sourceforge.net/spidermonkey/",
+      url = "http://code.google.com/p/python-spidermonkey/",
       long_description = """\
 Python/JavaScript bridge module, making use of Mozilla's spidermonkey
 JavaScript implementation.  Allows implementation of JavaScript classes,
@@ -29,12 +50,6 @@ objects and functions in Python, and evaluation and calling of JavaScript
 scripts and functions respectively.  Borrows heavily from Claes Jacobssen's
 Javascript Perl module, in turn based on Mozilla's 'PerlConnect' Perl binding.
 """,
-
-      ext_modules =  [Extension("spidermonkey",
-                                sources=["spidermonkey.pyx"],
-                                libraries=["js"],
-                                define_macros=macros)
-                      ],
-
+      ext_modules =  [ext],
       cmdclass = {'build_ext': build_ext}
       )
