1f367c9aad704535b4c308ba19649f692ccca228
varmaa <varmaa@e13cad08-5a4f-0410-891f-9d64462cff50>
2008-06-12 08:04:57 +0000

    Copied a subset of the /js/src directory from mozilla-central rev. 15323:1df6e4240511 into this repository to reduce dependencies; it's everything except xpconnect and liveconnect.  Integrated the building of Spidermonkey into the setup.py script, and also made some modifications to spidermonkey.pyx as a result of Spidermonkey no longer being compiled with JS_THREADSAFE. Modified README accordingly: all that needs to be done to build and install this module now is 'setup.py build' and 'setup.py install'.

--- setup.py
+++ setup.py
@@ -1,6 +1,10 @@
 import os
 import sys
+import glob
 from distutils.core import setup, Extension
+from distutils.command.clean import clean
+from distutils.command.install import install
+from distutils.file_util import copy_file
 
 try:
     # For some reason, importing Pyrex appears to change the value of
@@ -18,24 +22,59 @@ except ImportError:
     from distutils.command.build_ext import build_ext
     sources = ["spidermonkey.c"]
 
-if "build" in sys.argv or "build_ext" in sys.argv:
-    if not os.environ.has_key("MOZSDKDIR"):
-        print ("Please set the MOZSDKDIR environment variable "
-               "to the location of your XULRunner SDK before "
-               "running this script.")
-        sys.exit(1)
+def _find_obj_dir():
+    objdirs = [dirname for dirname
+               in glob.glob(os.path.join("js", "src", "*.OBJ"))
+               if os.path.isdir(dirname)]
+    if len(objdirs) > 1:
+        raise AssertionError("Multiple objdirs found in "
+                             "js dir: %s" % objdirs)
+    elif not objdirs:
+        raise AssertionError("No objdir found.")
+    return objdirs[0]
 
-MOZ_SDK_DIR = os.environ.get("MOZSDKDIR", "")
+class spidermonkey_install(install):
+    def run(self):
+        install.run(self)
+        objdir = _find_obj_dir()
+        if sys.platform == "darwin":
+            copy_file(os.path.join(objdir, "libjs.dylib"),
+                      "/usr/local/lib")
+        elif sys.platform.startswith("win"):
+            raise NotImplementedError("Windows not yet supported.")
+        else:
+            copy_file(os.path.join(objdir, "libjs.so"),
+                      "/usr/local/lib")
 
-ext = Extension(
-    "spidermonkey",
-    sources=sources,
-    include_dirs=[os.path.join(MOZ_SDK_DIR, "include"),
-                  os.path.join(MOZ_SDK_DIR, "include", "js")],
-    library_dirs=[os.path.join(MOZ_SDK_DIR, "lib")],
-    extra_compile_args=["-include", "mozilla-config.h"],
-    libraries=["mozjs"]
-    )
+class spidermonkey_clean(clean):
+    def run(self):
+        os.chdir(os.path.join("js", "src"))
+        os.system("make -f Makefile.ref clean")
+        os.chdir("../..")
+        clean.run(self)
+
+class spidermonkey_build_ext(build_ext):
+    def run(self):
+        os.chdir(os.path.join("js", "src"))
+        rv = os.system("make -f Makefile.ref")
+        os.chdir("../..")
+        if rv != 0:
+            print "Build failed, exiting."
+            sys.exit(rv)
+        objdir = _find_obj_dir()
+
+        self.include_dirs.extend([os.path.join("js", "src"), objdir])
+        self.library_dirs.append(objdir)
+
+        if self.define is None:
+            self.define = []
+        if sys.platform.startswith("win"):
+            raise NotImplementedError("Windows not yet supported.")
+        self.define.append(("XP_UNIX", None))
+        if sys.platform == "darwin":
+            self.define.append(("XP_MACOSX", None))
+
+        build_ext.run(self)
 
 setup(name = "spidermonkey",
       version = "0.0.1a",
@@ -51,6 +90,10 @@ objects and functions in Python, and evaluation and calling of JavaScript
 scripts and functions respectively.  Borrows heavily from Claes Jacobssen's
 Javascript Perl module, in turn based on Mozilla's 'PerlConnect' Perl binding.
 """,
-      ext_modules =  [ext],
-      cmdclass = {'build_ext': build_ext}
+      ext_modules =  [Extension("spidermonkey",
+                                sources=sources,
+                                libraries=["js"])],
+      cmdclass = {'build_ext': spidermonkey_build_ext,
+                  'clean': spidermonkey_clean,
+                  'install': spidermonkey_install}
       )
